e787d735de61da5f69a2e541deb3c79ed83d677b
epetutsc <ewald.petutschnig@gmail.com>
2021-05-02 00:08:08 +0200

    removed 'this.'

--- src/Main.Web/Startup.cs
+++ src/Main.Web/Startup.cs
@@ -13,7 +13,7 @@ namespace Main.Web
 
         public Startup(IConfiguration configuration)
         {
-            this._configuration = configuration;
+            _configuration = configuration;
         }
 
         // This method gets called by the runtime. Use this method to add services to the container.
